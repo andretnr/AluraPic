7aba2601e1602e18f82452fcc3a3d9c291e7585c
andre <andretnr@gmail.com>
2022-04-05 00:02:14 -0300

    Adicionando o Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PhotoComponent } from './photo/photo.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
