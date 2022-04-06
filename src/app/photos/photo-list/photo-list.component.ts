8f9745bb0ba30d0e4d89484330c9a4771ff6ee06
andre <andretnr@gmail.com>
2022-04-06 02:32:33 -0300

    Implementação dos cards

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,7 @@
 import { PhotoService } from './../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators'
+
 
 import { Photo } from './../photo/photo';
 
@@ -11,11 +10,10 @@ import { Photo } from './../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,14 +27,9 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params['userName'];
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
+
   }
 
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
 
   load() {
     this.photoService
@@ -47,10 +40,5 @@ export class PhotoListComponent implements OnInit, OnDestroy {
       });
   }
 
-  onKeyUp(target: any) {
-    if (target instanceof EventTarget) {
-      var elemento = target as HTMLInputElement;
-      this.debounce.next(this.filter = elemento.value);
-    }
-  }
+
 }
---
+++ src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+
+@Component({
+  selector: 'ap-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit {
+
+  debounce: Subject<string> = new Subject<string>();
+  filter: string = '';
+
+  ngOnInit(): void {
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  onKeyUp(target: any) {
+    if (target instanceof EventTarget) {
+      var elemento = target as HTMLInputElement;
+      this.debounce.next(this.filter = elemento.value);
+    }
+  }
+
+  }
